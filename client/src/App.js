9e561325a27aced2afa95755b202c59fa82fc36a
anupdhondi5 <anupdhondi5@gmail.com>
2020-12-22 00:11:13 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <div>Hello</div>
+            <div>Hello ksdnvkdsfv</div>
             <Navbar />
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
